8eae7367285da6c9c9d8448276bced77b99d57c0
GusProDev <102615870+GusProDev@users.noreply.github.com>
2024-11-04 03:45:31 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 // JavaScript to move the glow cursor with the mouse
-  document.addEventListener("mousemove", (e) => {
-    const body = document.querySelector("body");
-    body.style.setProperty("--cursor-x", e.clientX + "px");
-    body.style.setProperty("--cursor-y", e.clientY + "px");
-    body.classList.add("add-glow");
+document.addEventListener("mousemove", (e) => {
+  const body = document.querySelector("body");
+  body.style.setProperty("--cursor-x", e.clientX + "px");
+  body.style.setProperty("--cursor-y", e.clientY + "px");
+  body.classList.add("add-glow");
+});

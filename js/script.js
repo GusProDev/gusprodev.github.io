d97287494db46972187058a7ad69d644fced757e
GusProDev <102615870+GusProDev@users.noreply.github.com>
2024-11-04 03:42:49 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
-// script.js
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('JavaScript is linked and running!');
-});
+// JavaScript to move the glow cursor with the mouse
+  document.addEventListener("mousemove", (e) => {
+    const body = document.querySelector("body");
+    body.style.setProperty("--cursor-x", e.clientX + "px");
+    body.style.setProperty("--cursor-y", e.clientY + "px");
+    body.classList.add("add-glow");
